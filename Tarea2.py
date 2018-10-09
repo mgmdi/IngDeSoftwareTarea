818af8f8beb9fcaaaf08499e1b498f0dc4f116ca
mgmdi <14-10444@usb.ve>
2018-10-09 16:00:27 -0400

    Script basico

--- Tarea2.py
+++ Tarea2.py
@@ -1,18 +1,17 @@
-opcion = input("Ingrese la opcion 1 para ingresar datos por consola, ingrese dos para cargar un archivo de texto")
-
-
-
-    if(opcion == "1"):
-        edad = input("Imgrese fecha de nacimiento (XX/XX/XXXX): ")
-        sexo = input("Ingrese su sexo: ")
-        semCoti = input("Ingrese el numero de semanas cotizadas: ")
-        descontEdad = input("Ha trabajado usted en medios insalubres o capaces de producir vejez prematura?")
-        if(descontEdad = "Si" or descontEdad = "si"):
-            numAnos = input("Indique el numero de anos que trabajo: ")
-            if(numAnos > 4):
-                menosAnos = numAnos // 4
-                edad = edad - menosAnos
-                print("Se le ha descontado " + menosAnos + "de la edad considerada")
-    else if(opcion == "2"):
-        nombre = input("Ingrese nombre del archivo: ")
-        print(nombre)
+opcion = input("Ingrese la opcion 1 para ingresar datos por consola, ingrese dos para cargar un archivo de texto: ")
+if(opcion == "1"):
+    edad = input("Imgrese fecha de nacimiento (XX/XX/XXXX): ")
+    sexo = input("Ingrese su sexo: ")
+    semCoti = input("Ingrese el numero de semanas cotizadas: ")
+    descontEdad = input("Ha trabajado usted en medios insalubres o capaces de producir vejez prematura?")
+    if(descontEdad == "Si" or descontEdad == "si"):
+        numAnos = input("Indique el numero de anos que trabajo: ")
+        if(numAnos > 4):
+            menosAnos = numAnos // 4
+            edad = edad - menosAnos
+            print("Se le ha descontado " + menosAnos + "de la edad considerada")
+elif(opcion == "2"):
+    nombre = input("Ingrese nombre del archivo: ")
+    file = open(nombre, "r")
+    for line in file:
+        print(line)

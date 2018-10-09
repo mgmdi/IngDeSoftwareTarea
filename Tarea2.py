89cf896f4b2b693388123bb0d2b63f9f44b2bb4a
mgmdi <14-10444@usb.ve>
2018-10-09 15:38:32 -0400

    Script que solicita datos basicos

---
+++ Tarea2.py
@@ -0,0 +1,20 @@
+opcion = input("Ingrese la opcion 1 para ingresar datos por consola, ingrese dos para cargar un archivo de texto")
+
+switch(opcion){
+
+    case 1:
+        edad = input("Imgrese fecha de nacimiento (XX/XX/XXXX): ")
+        sexo = input("Ingrese su sexo: ")
+        semCoti = input("Ingrese el numero de semanas cotizadas: ")
+        descontEdad = input("Ha trabajado usted en medios insalubres o capaces de producir vejez prematura?")
+        if(descontEdad = "Si" or descontEdad = "si"):
+            numAnos = input("Indique el numero de anos que trabajo: ")
+            if(numAnos > 4):
+                menosAnos = numAnos // 4
+                edad = edad - menosAnos
+                print("Se le ha descontado " + menosAnos + "de la edad considerada")
+    case 2:
+        nombre = input("Ingrese nombre del archivo: ")
+        
+
+}

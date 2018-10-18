4e6547a4d42624f37e5c9657b4f51b25630d1201
mgmdi <14-10444@usb.ve>
2018-10-18 15:01:27 -0400

    clase para probar seguridad.py

---
+++ src/mysite/test_seguridad.py
@@ -0,0 +1,19 @@
+import unittest
+import seguridad as sec
+
+class TestBeta(unittest.TestCase):
+
+    
+    def test_registrarUsuario(self):
+        seguridad = sec.Seguridad()
+        self.assertEqual(seguridad.registrarUsuario("mgmdi","mjgf@gmail.com","mmM123MJ","mmM123MJ"), True)
+        self.assertEqual(seguridad.registrarUsuario("mgmdi","mjgf@gmail.com","mmM123MJ","mmMMJ"), False)
+        self.assertEqual(seguridad.registrarUsuario("mgmdi","mjgfgmail.com","mmM123MJ","mmM123MJ"), False)
+        self.assertEqual(seguridad.registrarUsuario("mgmdi","mjgf@gmail.com","mmM123MJ","mmM13MJ"), False)
+        self.assertEqual(seguridad.registrarUsuario("mgmdi","mjgf@gmail.com","123MJ","123MJ"), False)
+        self.assertEqual(seguridad.registrarUsuario("mgmdi","mjgf@gmail","123MJmmw","123MJmmw"), False)
+
+
+
+if __name__ == '__main__':
+    unittest.main() 
